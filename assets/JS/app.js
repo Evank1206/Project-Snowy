275a75cedb2fa6b4071ae4a39099a0be810c43dc
awwmicky <michael_fred_alvarez@yahoo.com>
2019-10-30 19:34:01 -0700

    firebase commit

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,14 +1,23 @@
 /* -------------------------------------------------------------------------- */
-$(() => {
 
-/* -------------------------------------------------------------------------- */
-// copy/paste : const = firebaseConfig
-/* -------------------------------------------------------------------------- */
+$(() => {
+/*  */
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAEgZVGKLpktoJVDCiHRoug_rLi7qjSTcg",
+    authDomain: "project-snowy.firebaseapp.com",
+    databaseURL: "https://project-snowy.firebaseio.com",
+    projectId: "project-snowy",
+    storageBucket: "project-snowy.appspot.com",
+    messagingSenderId: "111717425092",
+    appId: "1:111717425092:web:c99b2c8a86c02db87d0e91"
+};
+
 
 firebase.initializeApp(firebaseConfig);
 // database snapshot
 const database = firebase.database();
-// console.log(db);
+// console.log(database);
 
 
 
@@ -16,21 +25,6 @@ const database = firebase.database();
 
 
 
-// db.ref('data').on('value', snap => {
-//     console.log( snap.val() );
-// });
 
-// db.ref('data-2').set({
-//     letters: ['a', 'b', 'c', 'd']
-// });
-
-
-// db.ref('data-2').on('value', snap => {
-//     console.log( snap.val() );
-//     // $('span').text( snap.val().letters[0] );
-//     // document.write( snap.val().letters[0] );
-// });
-
-
-
+/*  */
 });
