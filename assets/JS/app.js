500f9603ac88ff5b6d8791c30010b1804d8eaeeb
Wilson Wong <wilsonwong415@gmail.com>
2019-10-30 20:45:28 -0700

    authuser

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -20,12 +20,53 @@ $(() => {
     // console.log(database);
     const auth = firebase.auth();
 
+    // create user
+    $(document).on('click', '#createUser', function () {
+        auth.createUserWithEmailAndPassword('#NewEmail', '#NewPassword');
+        console.log('created user');
+    });
 
+    auth.onAuthStateChanged(user => {
+        if (user) {
+            console.log('signed in');
+            $('#login').hide();
+            const logoutB = $('<button>').attr('id', 'logoutbutton');
+            logoutB.text('Logout')
+            $('#logoutbutton').append(logoutB);
+            $('#logoutbutton').show();
+        } else {
+            console.log('signed out');
+            $('#login').show();
+            $('.logoutbutton').empty();
+        }
+    });
 
+    $(document).on('click', '#logout', function () {
+        auth.signOut();
+    });
 
+    $('#textSubmit').on('click', function (e) {
+        e.preventDefault();
+        const email = $('#emailInput').val();
+        const password = $('#passwordInput').val();
+        auth.signInWithEmailAndPassword(email, password);
+    });
 
-
-
-
-    /*  */
+    document.onkeyup = e => {
+        if (e.key === 'Enter') {
+            auth.signInWithEmailAndPassword(email, password);
+        } else if (e.key === 'Escape') {
+            auth.signOut();
+        }
+    };
 });
+
+
+
+
+
+
+
+
+/*  */
+// });
